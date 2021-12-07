027a67c3d53782c66478d9659be8d0bd0888e520
Jadir Moreno <jadirsmoreno@gmail.com>
2021-12-07 15:14:06 -0300

    Teste de atualização

--- Alura/Py3/if.py
+++ Alura/Py3/if.py
@@ -3,23 +3,29 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+tentativas_total = 3
+contador = 0
+tentativas = 1
 
-chute = int(input("Digite o seu número: "))
-
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-while chute != numero_secreto:
-    entrada = int(input('Digite:\n[1] Continuar\n[2] Finalizar'))
+while tentativas <= tentativas_total:
     chute = int(input("Digite o seu número: "))
+    print('####Tentativa {} de {}####'.format(tentativas, tentativas_total))
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    contador +=1
+    tentativas += 1
+
     if acertou:
         print("Parabéns! Você acertou!")
+        print('Você acerto em {} tentativas'.format(contador))
+        break
     else:
         if maior:
             print("O seu chute foi maior do que o número secreto!")
         elif menor:
             print("O seu chute foi menor do que o número secreto!")
 
+
 print("Fim do jogo")
-print('aaaaaaaaaaaaaa')
