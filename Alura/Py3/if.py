2d68fddc1d48f0d6f8eeef379b5886862ced84ea
Jadir Moreno <jadirsmoreno@gmail.com>
2021-12-07 15:15:28 -0300

    Teste de atualização

--- Alura/Py3/if.py
+++ Alura/Py3/if.py
@@ -8,7 +8,7 @@ contador = 0
 tentativas = 1
 
 while tentativas <= tentativas_total:
-    chute = int(input("Digite o seu número: "))
+    chute = int(input('Digite o seu número: '))
     print('####Tentativa {} de {}####'.format(tentativas, tentativas_total))
 
     acertou = chute == numero_secreto
@@ -18,14 +18,14 @@ while tentativas <= tentativas_total:
     tentativas += 1
 
     if acertou:
-        print("Parabéns! Você acertou!")
+        print('Parabéns! Você acertou!')
         print('Você acerto em {} tentativas'.format(contador))
         break
     else:
         if maior:
-            print("O seu chute foi maior do que o número secreto!")
+            print('O seu chute foi maior do que o número secreto!')
         elif menor:
-            print("O seu chute foi menor do que o número secreto!")
+            print('O seu chute foi menor do que o número secreto!')
 
 
 print("Fim do jogo")
