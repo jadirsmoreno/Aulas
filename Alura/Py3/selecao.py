1eb19affd688178a0d5902556f7ffd85b3986323
Jadir Moreno <jadirsmoreno@gmail.com>
2022-05-11 16:09:37 -0300

    Random

--- Alura/Py3/selecao.py
+++ Alura/Py3/selecao.py
@@ -1,17 +1,20 @@
 import forca
 import numeroAleatorio
 
-print("*********************************")
-print("****** Escolha o seu jogo! ******")
-print("*********************************")
+def escolha_jogos():
+    print("*********************************")
+    print("****** Escolha o seu jogo! ******")
+    print("*********************************")
 
-print('(1) Forca\n(2) Adivinhação')
+    print('(1) Forca\n(2) Adivinhação')
 
-jogo = int(input('Qual o jogo?'))
+    jogo = int(input('Qual o jogo?'))
 
-if jogo == 1:
-    print('Jogando Forca!')
-    forca.jogar_forca()
-elif jogo == 2:
-    print('Jogando Adivinhação!')
-    numeroAleatorio.jogar_numero_aleatorio()
+    if jogo == 1:
+        print('Jogando Forca!')
+        forca.jogar_forca()
+    elif jogo == 2:
+        print('Jogando Adivinhação!')
+        numeroAleatorio.jogar_numero_aleatorio()
+if(__name__ == "__main__"):
+    escolha_jogos()
